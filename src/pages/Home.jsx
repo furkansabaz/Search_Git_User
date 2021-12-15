bfb26f51cb692ec41ea01a323c1fad9340074808
Furkan Sabaz <furkan.sabaz@gmail.com>
2021-12-15 14:07:40 +0300

    First Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResult from "../components/users/UserResult"
+
+function Home() {
+  return (
+    <>
+    {/* Search Component */}
+      <UserResult/>
+    </>
+  )
+}
+
+export default Home
